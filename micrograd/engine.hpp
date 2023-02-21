2a5e1c092f34bfafb1c871694056e361f6b6b580
Jac-Zac <jacopozac@icloud.com>
2023-02-21 14:10:36 +0100

    m_back wokring

--- micrograd/engine.hpp
+++ micrograd/engine.hpp
@@ -10,52 +10,51 @@
 
 #include <array>
 #include <cmath>
+#include <functional>
+#include <iostream>
 #include <memory>
 #include <string>
-#include <iostream>
-#include <functional>
 
 template <typename T> class Value {
 public:
-    T data; // data of the value
-    mutable T grad; //gradient which by default is zero and it is mutable
+    T data;            // data of the value
+    mutable T grad;    // gradient which by default is zero and it is mutable
     std::string label; // label of the value
     std::function<void()> m_backward; // lambda function
 public:
     // Constructor
     Value(T data, std::string label = "", std::string op = "",
-          std::array<std::shared_ptr<Value<T>>, 2> children = {nullptr,
-                                                               nullptr})
-        : data(data), label(label), m_op(op), m_prev((std::move(children))),
-          grad(0) {
-              // Initialize the lambda to none
-              m_backward = [](){};
-          }
+          std::array<std::shared_ptr<Value<T>>, 2> children = {})
+        : data(data), label(label), m_op(op), m_prev{std::move(children)}, grad(0) {
+        // Initialize the lambda to none
+        m_backward = []() {};
+    }
 
     // Operator Overloading
     Value operator+(Value const &other) const;
-    Value operator*(Value const &ohter) const;
+    Value operator*(Value const &other) const;
 
     Value tanh();
 
     // << operator overload
-    friend std::ostream &operator<<(std::ostream &os, const Value &v){
-        os << "Value(data=" << v.data << ", grad=" << v.grad << ", label=" << v.label << ")";
+    friend std::ostream &operator<<(std::ostream &os, const Value &v) {
+        os << "Value(data=" << v.data << ", grad=" << v.grad
+           << ", label=" << v.label << ")";
         return os;
     };
 
     void get_prev() const;
 
-protected:
+public:
     std::string m_op;
     std::array<std::shared_ptr<Value<T>>, 2> m_prev;
 };
 
 template <typename T>
 Value<T> Value<T>::operator+(Value<T> const &other) const {
-    auto out = Value(data + other.data, "", "+",
-                        {std::make_shared<Value>(*this),
-                         std::make_shared<Value>(other)});
+    auto out =
+        Value(data + other.data, "", "+",
+              {std::make_shared<Value<T>>(*this), std::make_shared<Value<T>>(other)});
     out.m_backward = [&]() mutable {
         // Should just move the gradient along to both of them
         this->grad = 1.0 * out.grad;
@@ -66,9 +65,9 @@ Value<T> Value<T>::operator+(Value<T> const &other) const {
 
 template <typename T>
 Value<T> Value<T>::operator*(Value<T> const &other) const {
-    auto out = Value(data * other.data, "" , "*",
-                        {std::make_shared<Value>(std::move(*this)),
-                         std::make_shared<Value>(std::move(other))});
+    auto out =
+        Value(data * other.data, "", "*",
+              {std::make_shared<Value<T>>(*this), std::make_shared<Value<T>>(other)});
     out.m_backward = [&]() mutable {
         this->grad += other.data * out.grad;
         other.grad += this->data * out.grad;
@@ -77,24 +76,23 @@ Value<T> Value<T>::operator*(Value<T> const &other) const {
 }
 
 // Function to get the previous elements that make up this element
-template <typename T>
-Value<T> Value<T>::tanh(){
+template <typename T> Value<T> Value<T>::tanh() {
     T x = this->data;
-    T t = (exp(2 * x) - 1)/(exp(2 * x) + 1);
-    auto out = Value(t, "" , "tanh", {std::make_shared<Value>(*this), nullptr});
+    T t = (exp(2 * x) - 1) / (exp(2 * x) + 1);
+    auto out = Value(t, "", "tanh", {std::make_shared<Value<T>>(*this), nullptr});
     out.m_backward = [this, &out, t]() mutable {
         // Chain rule with the derivative of tanh
-        this->grad = (1 - pow(t,2)) * out.grad;
+        this->grad = (1 - pow(t, 2)) * out.grad;
     };
     return out;
 }
 
 // Function to get the previous elements that make up this element
-template <typename T>
-void Value<T>::get_prev() const {
-    if (this->m_prev[1] != nullptr){
-        std::cout << "{" << *(this->m_prev[0]) << "," << *(this->m_prev[1]) << "}\n";
-    }else{
+template <typename T> void Value<T>::get_prev() const {
+    if (this->m_prev[1] != nullptr) {
+        std::cout << "{" << *(this->m_prev[0]) << "," << *(this->m_prev[1])
+                  << "}\n";
+    } else {
         std::cout << "{" << *(this->m_prev[0]) << "}\n";
     }
 }
--- test/main.cpp
+++ test/main.cpp
@@ -8,14 +8,14 @@ int main() {
     auto x1 = Value<double>(2.0, "x1");
     auto x2 = Value<double>(0.0, "x2");
     // Weight w1, w2
-    auto w1 = Value<double>(-3.0, "w2");
-    auto w2 = Value<double>(1.0, "w1");
+    auto w1 = Value<double>(-3.0, "w1");
+    auto w2 = Value<double>(1.0, "w2");
 
     // products
-    auto x1w1 = x1*w1;
+    auto x1w1 = x1 * w1;
     x1w1.label = "x1*w1";
-    auto x2w2 = x2*w2;
-    x1w1.label = "x2*w2";
+    auto x2w2 = x2 * w2;
+    x2w2.label = "x2*w2";
 
     // sum of the two
     auto x1w1_x2w2 = x1w1 + x2w2;
@@ -36,12 +36,25 @@ int main() {
     // Call backward
     o.m_backward();
     n.m_backward();
+    b.m_backward();
+    x1w1_x2w2.m_backward();
+    x1w1.m_backward();
+    x2w2.m_backward();
+
+//    std::cout << "Children count= " << o.m_prev[0].use_count() << "\n";
+
+//   n.data = 100;
 
     std::cout << o << "\n";
-    std::cout << "O prev: ";
-    o.get_prev();
-    std::cout << "\n";
     std::cout << n << "\n";
+    std::cout << b << "\n";
+    std::cout << x1w1_x2w2 << "\n";
+    std::cout << x1w1<< "\n";
+    std::cout << x2w2<< "\n";
+    std::cout << w2<< "\n";
+    std::cout << x2<< "\n";
+    std::cout << x1<< "\n";
+    std::cout << w1<< "\n";
 
     return 0;
 }
