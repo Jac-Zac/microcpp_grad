ad8244b3772bf848f0aecd4be5e07c401c2d2f08
Jac-Zac <jacopozac@icloud.com>
2023-02-24 14:27:08 +0100

    changing protected members

--- micrograd/engine.hpp
+++ micrograd/engine.hpp
@@ -28,8 +28,6 @@ enum value_ops : unsigned char {
 
 template <typename T> class Value {
 public:
-    T data;            // data of the value
-    T grad;            // gradient which by default is zero
     std::string label; // label of the value
 public:
     // Constructor
@@ -53,7 +51,7 @@ public:
 
     // << operator overload
     friend std::ostream &operator<<(std::ostream &os, const Value<T> &v) {
-        os << "Value(data=" << v.data << ", grad=" << v.grad
+        os << "Value(data=" << v.data << ", grad=" << v.m_grad
            << ", label=" << v.label << ")";
         return os;
     };
@@ -68,10 +66,12 @@ public:
     void print_graph();
 
 protected:
+    T data;                                   // data of the value
     char m_op;
-    std::array<Value<T> *, 2> m_prev; // previous values
+    T m_grad;                                 // gradient which by default is zero
+    std::array<Value<T> *, 2> m_prev;         // previous values
     std::vector<Value<T> *>
-        m_sorted_values; // vector to store the sorted values
+    m_sorted_values;                          // vector to store the sorted values
     std::unordered_set<Value<T> *> m_visited; // keep track of the visited nodes
 protected:
     // Helper function to make a topological sort
@@ -83,7 +83,7 @@ protected:
 
 template <typename T>
 Value<T>::Value(T data, std::string label, char op)
-    : data(data), label(label), m_op(op), grad(0), m_prev({nullptr, nullptr}) {}
+    : data(data), label(label), m_op(op), m_grad(0), m_prev({nullptr, nullptr}) {}
 
 template <typename T> Value<T> Value<T>::operator+(Value<T> &other) {
     Value<T> result = Value<T>(data + other.data, "", SUM);
@@ -182,37 +182,37 @@ template <typename T> void Value<T>::_backward() {
     case SUM:
         // Should just move the gradient along to both of them
         // += because we want to avoid bugs if we reuse a variable
-        this->m_prev[0]->grad += 1.0 * this->grad;
-        this->m_prev[1]->grad += 1.0 * this->grad;
+        this->m_prev[0]->m_grad += 1.0 * this->m_grad;
+        this->m_prev[1]->m_grad += 1.0 * this->m_grad;
         break;
     case DIF:
-        this->m_prev[0]->grad += 1.0 * this->grad;
-        this->m_prev[1]->grad -= 1.0 * this->grad;
+        this->m_prev[0]->m_grad += 1.0 * this->m_grad;
+        this->m_prev[1]->m_grad -= 1.0 * this->m_grad;
         break;
     case MUL:
-        this->m_prev[0]->grad += this->m_prev[1]->data * this->grad;
-        this->m_prev[1]->grad += this->m_prev[0]->data * this->grad;
+        this->m_prev[0]->m_grad += this->m_prev[1]->data * this->m_grad;
+        this->m_prev[1]->m_grad += this->m_prev[0]->data * this->m_grad;
         break;
     case DIV:
-        this->m_prev[0]->grad += (1 / (this->m_prev[1]->data)) * this->grad;
-        this->m_prev[1]->grad -= (this->m_prev[0]->data) /
-                                 pow(this->m_prev[1]->data, 2) * this->grad;
+        this->m_prev[0]->m_grad += (1 / (this->m_prev[1]->data)) * this->m_grad;
+        this->m_prev[1]->m_grad -= (this->m_prev[0]->data) /
+                                 pow(this->m_prev[1]->data, 2) * this->m_grad;
         break;
     case POW:
-        this->m_prev[0]->grad +=
+        this->m_prev[0]->m_grad +=
             (this->m_prev[1]->data *
              pow(this->m_prev[0]->data, (this->m_prev[1]->data - 1))) *
-            this->grad;
+            this->m_grad;
         break;
     case EXP:
         // e^x is e^x which I already saved in this->data
-        this->m_prev[0]->grad += this->data * this->grad;
+        this->m_prev[0]->m_grad += this->data * this->m_grad;
         break;
     case TANH:
-        this->m_prev[0]->grad += (1 - pow(this->data, 2)) * this->grad;
+        this->m_prev[0]->m_grad += (1 - pow(this->data, 2)) * this->m_grad;
         break;
     case RELU:
-        // this->m_prev[0]->grad += (1 - pow(this->data, 2)) * this->grad;
+        // this->m_prev[0]->m_grad += (1 - pow(this->data, 2)) * this->m_grad;
         break;
     default:
         break;
@@ -242,7 +242,7 @@ template <typename T> void Value<T>::backward() {
     }
 
     // Set the derivative of dx/dx to 1
-    this->grad = 1.0;
+    this->m_grad = 1.0;
 
     // Call backward in topological order applying the chain rule automatically
     for (auto &value : m_sorted_values) {
@@ -250,30 +250,27 @@ template <typename T> void Value<T>::backward() {
     }
 }
 
-template <typename T> void Value<T>::print_graph() {
-    // If empty do topo sort
+template <typename T>
+void Value<T>::print_graph(){
+    // Perform a topological sort of the graph
     if (m_sorted_values.empty()) {
         _topo_sort(this);
         std::reverse(m_sorted_values.begin(), m_sorted_values.end());
     }
 
-#define ASCII_DRAWING 1
-#if ASCII_DRAWING == 1
-    for (auto &value : m_sorted_values) {
-        std::cout << *value;
-        if (value->m_op != ' ') {
-            std::cout << '\n'
-                      << std::string(20, ' ') << "|" << '\n'
-                      << std::string(20, ' ') << value->m_op
-                      << std::string(10, ' ') << '\n'
-                      << std::string(20, ' ') << "|" << '\n';
-        } else {
-            std::cout << '\n';
+    // Print out the graph
+    std::cout << "digraph G {\n";
+    for (const auto &values : m_sorted_values) {
+        std::cout << "  " << values->label << " [label=\"" << values->label << " | " << values->data << " | " << values->m_grad << "\", shape=record]\n";
+        for (const auto &prev_value : values->m_prev) {
+            if (prev_value != nullptr) {
+                std::cout << "  " << *prev_value << " -> " << *values << "\n";
+            }
         }
     }
-#else
-    for (auto &value : m_sorted_values) {
-        std::cout << *value << '\n';
+    std::cout << "}\n";
+
+    for (const auto &values : m_sorted_values){
+        std::cout << *values << '\n';
     }
-#endif
 }
