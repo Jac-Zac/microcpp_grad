3003b72f1cabc0a0f1e0bcf343cb224c48e409d0
Jac-Zac <jacopozac@icloud.com>
2023-02-28 00:49:51 +0100

    single neuron

--- examples/ops_example.cpp
+++ examples/ops_example.cpp
@@ -1,43 +1,44 @@
-/* #include "../micrograd/engine.hpp" */
-/*  */
-/* int main() { */
-/*     // Creating a single perception */
-/*  */
-/*     // Input x1, x2 */
-/*     auto x1 = Value<double>(2.0, "x1"), x2 = Value<double>(0.0, "x2"); */
-/*     // Weight w1, w2 */
-/*     auto w1 = Value<double>(-3.0, "w1"), w2 = Value<double>(1.0, "w2"); */
-/*  */
-/*     // products */
-/*     auto x1w1 = x1 * w1; */
-/*     x1w1.label = "x1*w1"; */
-/*     auto x2w2 = x2 * w2; */
-/*     x2w2.label = "x2*w2"; */
-/*  */
-/*     // sum of the two */
-/*     auto x1w1_x2w2 = x1w1 + x2w2; */
-/*     x1w1_x2w2.label = "x1w1 + x2w2"; */
-/*  */
-/*     // Bias of the neuron b */
-/*     auto b = Value<double>(6.8813735870195432, "b"); */
-/*  */
-/*     // new neuron */
-/*     auto n = (x1w1_x2w2 + b); */
-/*     n.label = "n"; */
-/*  */
-/*     // auto o = n.tanh(); */
-/*  */
-/*     // Custom tanh implementation */
-/*     auto e = (n * 2).exp_value(); */
-/*     e.label = "e"; */
-/*     auto o = (e - 1) / (e + 1); */
-/*     o.label = "o"; */
-/*  */
-/*     // Grandina with respect to itself is 1 */
-/*     o.backward(); */
-/*     o.draw_graph(); */
-/* } */
+#include "../micrograd/engine.hpp"
 
+int main() {
+    // Creating a single perception
+
+    // Input x1, x2
+    auto x1 = Value<double>(2.0, "x1"), x2 = Value<double>(0.0, "x2");
+    // Weight w1, w2
+    auto w1 = Value<double>(-3.0, "w1"), w2 = Value<double>(1.0, "w2");
+
+    // products
+    auto x1w1 = x1 * w1;
+    x1w1.label = "x1*w1";
+    auto x2w2 = x2 * w2;
+    x2w2.label = "x2*w2";
+
+    // sum of the two
+    auto x1w1_x2w2 = x1w1 + x2w2;
+    x1w1_x2w2.label = "x1w1 + x2w2";
+
+    // Bias of the neuron b
+    auto b = Value<double>(6.8813735870195432, "b");
+
+    // new neuron
+    auto n = (x1w1_x2w2 + b);
+    n.label = "n";
+
+    // auto o = n.tanh();
+
+    // Custom tanh implementation
+    auto e = (n * 2).exp_value();
+    e.label = "e";
+    auto o = (e - 1) / (e + 1);
+    o.label = "o";
+
+    // Grandina with respect to itself is 1
+    o.backward();
+    o.draw_graph();
+}
+
+/*
 #include "../micrograd/engine.hpp"
 
 int main() {
@@ -50,16 +51,17 @@ int main() {
     auto mid1 = c;
     mid1.label = "mid1";
     c = mid1 + (mid1 + 1);
-    /* auto mid2 = c; */
-    /* mid2.label = "mid2"; */
-    /* c = mid2 + ((mid2 - a) + 1); */
-    /* auto tmp1 = d; */
-    /* tmp1.label = "tmp1"; */
-    /* d = tmp1 + ((tmp1 * 2) + (b + a).relu()); */
-    /* auto tmp2 = d; */
-    /* tmp2.label = "tmp2"; */
-    /* d = tmp2 + (3 * tmp2 + (b - a).relu()); */
-    /* d.label = "d"; */
+
+    // auto mid2 = c;
+    // mid2.label = "mid2";
+    // c = mid2 + ((mid2 - a) + 1);
+    // auto tmp1 = d;
+    // tmp1.label = "tmp1";
+    // d = tmp1 + ((tmp1 * 2) + (b + a).relu());
+    // auto tmp2 = d;
+    // tmp2.label = "tmp2";
+    // d = tmp2 + (3 * tmp2 + (b - a).relu());
+    // d.label = "d";
     auto e = c - d;
     e.label = "e";
     auto f = e ^ 2;
@@ -74,3 +76,4 @@ int main() {
     std::cout << a << '\n';
     std::cout << b << '\n';
 }
+*/
--- test/main.cpp
+++ test/main.cpp
@@ -30,14 +30,12 @@ int main() {
     /*     std::cout << value << '\n'; */
     /* } */
 
-    Value<TYPE> loss = Value<TYPE>(0, "loss");
-
-    Value<TYPE> tmp = Value<TYPE>(0, "tmp");
+    auto loss = Value<TYPE>(0, "loss");
+    auto tmp = loss;
 
     for (size_t i = 0; i < 4; i++) {
         // Mean Squared Error
-        tmp = (ypred[i] - ys[i]);
-        loss += ((ypred[i] - ys[i]) ^ 2);
+        loss = tmp + ((ypred[i] - ys[i]) ^ 2);
     }
 
     std::cout << n.m_layers[0].m_neurons[0].m_weights[0] << '\n';
