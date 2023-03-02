dee56fee623b0e2c7474e01ec23a37bdf9d333c7
Jac-Zac <jacopozac@icloud.com>
2023-03-03 00:05:33 +0100

    have to rethink things

--- examples/neural_net.cpp
+++ examples/neural_net.cpp
@@ -1,18 +1,30 @@
 #include <micrograd/nn.hpp>
 
+using namespace nn;
+
 int main() {
     /// Initialize the neural network
     auto model = MLP<double, 3>(3, {4, 4, 1});
 
-    std::vector<Value<double>> x = {
-        Value<double>(2.0, "first_value"),
-        Value<double>(3.0, "second_value"),
-        Value<double>(-1.0, "third_value"),
+    auto x = std::vector<std::shared_ptr<Value<double>>>{
+        std::make_shared<Value<double>>(2.0),
+        std::make_shared<Value<double>>(3.0),
+        std::make_shared<Value<double>>(-1.0)
     };
 
     // auto will be an std::variant
     auto y = model(x);
 
-    std::get<Value<double>>(y).backward();
-    std::get<Value<double>>(y).draw_graph();
+    // 0 because we only have 1 neuron
+    y[0]->backward();
+    y[0]->draw_graph();
+
+    for (auto &p : model.parameters()) {
+        std::cout << p << "\n";
+    }
+
+    std::cout << "Model information: " << model << "\n";
+    std::cout << "Number of parameters: " << model.parameters().size() << "\n";
+
+    std::cout << "Output: " << *y[0] << "\n";
 }
